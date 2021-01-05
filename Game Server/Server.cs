c50454ba991825f1ba95437cbb185a3d55b74f20
gmanspaceman <gman4537@gmail.com>
2021-01-05 14:31:51 -0800

    Update Server.cs

--- Game Server/Server.cs
+++ Game Server/Server.cs
@@ -116,7 +116,7 @@ namespace Game_Server
                     if (!stream.DataAvailable)
                     {
                         Thread.Sleep(125); //slowdown thread
-                        continue;
+                        //continue;
                     }
                     lastComm.Restart();
 
